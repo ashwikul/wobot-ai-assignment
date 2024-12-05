4a60f083202cec2cd04513759a24b5a79d51c526
ashwikul <ashwinikk1990@gmail.com>
2024-12-05 15:04:33 +0530

    fetch data from api

--- src/components/CameraList.js
+++ src/components/CameraList.js
@@ -1,7 +1,145 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import cloud from "../assets/cloud.svg";
+import device from "../assets/device.svg";
+import actions from "../assets/actions.svg";
 
 function CameraList() {
-  return <div></div>;
+  const [data, setData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
+  useEffect(() => {
+    const url = process.env.REACT_APP_API_URL;
+    const token = process.env.REACT_APP_API_TOKEN;
+
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json()) // Parse the JSON data from the response
+      .then((data) => {
+        console.log("Data received:", data);
+        setData(data.data);
+        setDisplayData(data.data);
+      })
+      .catch((err) => console.log("error", err));
+  }, []);
+  const updateData = (id, status) => {};
+  const deleteData = (id, status) => {};
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>NAME</th>
+          <th>HEALTH</th>
+          <th>LOCATION</th>
+          <th>RECORDER</th>
+          <th>TASKS</th>
+          <th>STATUS</th>
+          <th>ACTIONS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {displayData.map((item) => {
+          return (
+            <tr key={item.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
+
+              <td>
+                <div
+                  style={{
+                    display: "flex",
+                    gap: "5px",
+                    alignItems: "baseline",
+                  }}
+                >
+                  <div
+                    className={item.current_status.toLowerCase()}
+                    style={{
+                      width: "10px",
+                      height: "10px",
+                      borderRadius: "5px",
+                    }}
+                  ></div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "3px",
+                    }}
+                  >
+                    <div
+                      style={{
+                        fontFamily: "Inter",
+                        fontSize: "14px",
+                        fontWeight: "400",
+                        lineHeight: " 16.94px",
+                      }}
+                    >
+                      {item.name}
+                    </div>
+                    <div
+                      style={{
+                        fontFamily: "Inter",
+                        fontSize: "12px",
+                        fontWeight: 400,
+                        lineHeight: "14.52px",
+                      }}
+                    >
+                      {item.location}
+                    </div>
+                  </div>
+                </div>
+              </td>
+
+              <td>
+                <div
+                  style={{
+                    display: "flex",
+                    textAlign: "left",
+                  }}
+                >
+                  <div>
+                    <img src={cloud} />
+                    {item.health.cloud}
+                  </div>
+                  <div>
+                    <img src={device} />
+                    {item.health.device}
+                  </div>
+                </div>
+              </td>
+              <td>{item.location}</td>
+              <td>{item.recorder}</td>
+              <td>{item.tasks} Tasks</td>
+              <td>
+                <button
+                  className={item.status.toLowerCase()}
+                  onClick={() => updateData(item.id, item.status)}
+                >
+                  {item.status}
+                </button>
+              </td>
+              <td>
+                <div
+                  style={{ cursor: "pointer" }}
+                  onClick={() => deleteData(item.id, item.status)}
+                >
+                  <img src={actions} />
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default CameraList;
